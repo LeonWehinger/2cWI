0cd9f99febfcc527ec3e8ec8c532d8a785ece4b1
Leon <leon.wehinger@htldornbirn.at>
2023-12-07 09:05:13 +0100

    01_JS finished

--- Javascript/01_JS/script.js
+++ Javascript/01_JS/script.js
@@ -1,14 +1,71 @@
 console.log("Hallo");
 console.log("Luca mag Füße");
 
-// Variablen
-let a = 888
-let b = 8
 
-console.log("a hat den wert" + a)
 
-a = 1
-console.log("Jetzt hat a den wert" + a)
 
-let result =  b - a;
-console.log("b - a = " + result);
+// Variablen 
+
+let a= 7; 
+
+let b = 8; 
+
+let c = 10; 
+
+// Berechne folgendes und gib das Eregebnis über console.log aus 
+
+// a + b + c 
+let y = a + b + c;
+console.log("a + b + c = " +y);
+
+// (a + b) / c 
+
+let z = (a + b) / c
+console.log("(a + b) / c = " +z);
+
+// c -a + b 
+
+let v = c - a + b
+console.log("c - a + b = " +v);
+
+// a + b + c * 3 
+
+let n = a + b + c * 3
+console.log("a + b + c * 3 = " +n);
+
+// (a + b + c)/4 
+
+let m = (a + b + c) / 4
+console.log("(a + b + c) / 4 = " +m);
+// Ändere die Variablen auf folgende Werte 
+
+// a = 3 
+
+// b = a + 3 
+
+// c = c -3 
+
+// gib die neuen Werte der Variablen aus 
+
+a = 3;
+b = a + 3;
+c = c - 3;
+
+let ü = a + b + c;
+console.log("a + b + c = " +ü);
+
+
+let ä = (a + b) / c
+console.log("(a + b) / c = " +ä);
+
+
+let ö = c - a + b
+console.log("c - a + b = " +ö);
+
+
+let p = a + b + c * 3
+console.log("a + b + c * 3 = " +p);
+
+
+let o = (a + b + c) / 4
+console.log("(a + b + c) / 4 = " +o);
