fdf66b9a4c578b6de3766869b9dd48d1fe4c379d
Leon <leon.wehinger@htldornbirn.at>
2023-12-07 09:21:06 +0100

    02_JS finished

---
+++ Javascript/02 JS/script.js
@@ -0,0 +1,16 @@
+//Berechne das Ergebnis von 17.4 / 3.9. Gib folgenden Satz aus: 
+
+//The result of 17.4 divided by 3.9 is 4.461538461538462 
+
+y = 17.4 / 3.9;
+console.log("The result of 17.4 divided by 3.9 is " +y)
+
+//Lege eine Variable firstname und eine Varialbe lastname an. Weise den Variablen Deinen Namen zu. Gib folgendes aus: 
+
+//"Ich heiße VORNAME NACHNAME" 
+
+let firstname = "Leon";
+let lastname = "Wehinger";
+console.log("Ich heiße " + firstname + " " + lastname);
+
+
