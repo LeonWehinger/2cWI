9060fdfebc66041dbc500529fa5d70c1bc69ec86
Leon <leon.wehinger@htldornbirn.at>
2023-12-07 08:29:17 +0100

    changes RAN

---
+++ Javascript/01_JS/script.js
@@ -0,0 +1,14 @@
+console.log("Hallo");
+console.log("Luca mag Füße");
+
+// Variablen
+let a = 888
+let b = 8
+
+console.log("a hat den wert" + a)
+
+a = 1
+console.log("Jetzt hat a den wert" + a)
+
+let result =  b - a;
+console.log("b - a = " + result);
