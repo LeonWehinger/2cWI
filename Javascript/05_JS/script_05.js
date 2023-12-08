6292884246616dc4abc955280b348ceb65bda29b
Leon <leon.wehinger@htldornbirn.at>
2023-12-08 16:12:13 +0100

    06_JS finished

---
+++ Javascript/05_JS/script_05.js
@@ -0,0 +1,35 @@
+// Aufgabe 1 
+
+// Wenn die Zahl kleiner ist als 20  gib aus "Mini" 
+
+// Wenn die Zahl zw. 20 und 50 ist gib aus "Medium" 
+
+// Wenn die Zahl größer als 50 ist gib aus "Large" 
+
+let Zahl1 = 17;
+if (Zahl1 < 20) {
+    console.log("Mini");
+} else if (Zahl1 < 51) {
+    console.log("Medium");
+} else {
+    console.log("Large")
+}
+
+
+
+// Aufgabe 2
+
+// Wenn die Zahl kleiner gleich 7 ist gib aus "Mini"
+
+// Wenn die Zahl zwischen 7 und 90 ist gib  aus "Medium"
+
+// Wenn die Zahl größer als 90 ist gib aus "Large" 
+
+let Zahl2 = 89
+if (Zahl2 <= 7) {
+    console.log("Mini");
+} else if (Zahl2 < 91) {
+    console.log("Medium");
+} else {
+    console.log("Large");
+}
