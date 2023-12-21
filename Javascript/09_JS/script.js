853fd79254827764a9fb574399c152da664d4a43
Leon <leon.wehinger@htldornbirn.at>
2023-12-21 11:18:22 +0100

    09_JS 2/3 finished

---
+++ Javascript/09_JS/script.js
@@ -0,0 +1,32 @@
+for (let Zahl = 1; Zahl < 32; Zahl++) {
+
+    if (Zahl % 2 == 0) {
+        console.log(" ")
+    }
+
+    else if (Zahl == 8 || Zahl == 16 || Zahl == 24) {
+        console.log("/n")
+    }
+
+    else if (Zahl > 0 && Zahl < 9 || Zahl > 15 && Zahl < 25) {
+        console.log("X")
+    }
+
+
+    else if (Zahl > 7 && Zahl < 17 || Zahl > 23 && Zahl < 32) {
+        console.log("O");
+    }
+
+}
+
+let counter = 0;
+
+for (let i = 0; i <= 100; i++) {
+    if (i % 2 == 0) {
+       counter += i;
+
+    }
+
+}
+
+console.log(counter);
