186ac2207ede4b2f49313fa9175f7d099af05426
Leon <leon.wehinger@htldornbirn.at>
2024-01-18 08:42:35 +0100

    11_JS finished

--- Javascript/11_JS/script.js
+++ Javascript/11_JS/script.js
@@ -1,6 +1,6 @@
 let arr = [4, 1, 2, 3]
-arr.push (17)
-arr.push (199)
+arr.push (17);
+arr.push (199);
 
 
 for (i = 0; i < arr.length; i++){
@@ -13,3 +13,9 @@ for (i = 0; i < arr.length; i++){
 
 let mittelwert = c/ arr.length;
 console.log(mittelwert);
+
+
+let arr2 = ["Susi", "Paula", "Hans"]
+console.log("Meine Freunde sind " +arr2)
+arr2.push ("Sepp");
+console.log("Meine Freunde sind " +arr2[0] + ", " + arr2[1] + ", " +arr2[2] + " und " +arr2[3])
