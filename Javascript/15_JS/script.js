b1eb850933d24284d0beedff822555a11f10410f
Leon <leon.wehinger@htldornbirn.at>
2024-04-25 08:00:39 +0200

    End lesson

---
+++ Javascript/15_JS/script.js
@@ -0,0 +1,31 @@
+let field = [
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+];
+
+//printField();
+
+
+
+
+
+function printField(){
+    for (let row = 0; row< field.length; row++) {
+        let actualRow = field[row];
+        let output = "";
+        for (let col = 0; col < actualRow.length; col++) {
+            let sign=" ";
+            if (actualRow[col]==1){
+                sign= "X";
+            }   else if (actualRow [col]==2){
+                sign = "O";
+            }
+
+            output += actualRow[col] + "|"
+        }
+        console.log(output);
+    }
+}
+
+
