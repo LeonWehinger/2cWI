b49365d5b08a32a65cac40dd5a633d7c017e5871
Leon <leon.wehinger@htldornbirn.at>
2024-06-06 09:30:16 +0200

    Lesson_End

--- Javascript/15_JS/script.js
+++ Javascript/15_JS/script.js
@@ -31,20 +31,24 @@ let field = [
 
 function printField() {
 
-    for (let row = 0; row < field.length; row++) {
-        let actualRow = field[row];
-        let output = "";
-        for (let col = 0; col < actualRow.length; col++) {
-            let sign = " ";
-            if (actualRow[col] == 1) {
-                sign = "X";
-            } else if (actualRow[col] == 2) {
-                sign = "O";
-            }
+    for (let turn = 1; turn <= 2; turn++) {
+        for (let row = 0; row < field.length; row++) {
+            let actualRow = field[row];
+            let output = "";
+            for (let col = 0; col < actualRow.length; col++) {
+                let sign = " ";
+                if (actualRow[col] == 1) {
+                    sign = "X";
+                } else if (actualRow[col] == 2) {
+                    sign = "O";
+                }
 
-            output += actualRow[col] + "|"
+                output += actualRow[col] + "|"
+            }
+            console.log(output);
+
         }
-        console.log(output);
+        let choice = await readLineAsync();
 
     }
 }
